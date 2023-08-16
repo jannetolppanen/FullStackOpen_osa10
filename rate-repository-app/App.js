41e8c0d8cad3ee77e042eff6dc3f47c10a5d77b7
Janne Tolppanen <janne.ks@gmail.com>
2023-08-16 21:22:59 +0300

    9.2 or something

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('message')  
+  console.log('yeet')
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
   );
